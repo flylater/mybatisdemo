a9a6ed4fe644e346cd34c78a5fae4da2f7dfbe81
cqrd_rszhang <rszhang@travelsky.com>
2020-09-11 11:08:09 +0800

    Redis序列化，使用Jackson2JsonRedisSerializer和GenericJackson2JsonRedisSerializer

--- mybatisdemo/src/main/java/com/rszhang/mybatisdemo/config/RedisConfig.java
+++ mybatisdemo/src/main/java/com/rszhang/mybatisdemo/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package com.rszhang.mybatisdemo.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.rszhang.mybatisdemo.bean.Employee;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +14,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -17,14 +23,49 @@ import java.util.Objects;
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
 
+//    /**
+//     *  使用json序列化，使用Jackson2JsonRedisSerializer，当数据对象中包含另外的对象时反序列有问题
+//     * @param redisConnectionFactory
+//     * @return
+//     */
+//    @Bean
+//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//
+//        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory);
+//        template.setDefaultSerializer(serializer);
+//        return template;
+//    }
+
+//    /**
+//     *  使用json序列化，使用Jackson2JsonRedisSerializer，解决了反序列化问题
+//     * @param redisConnectionFactory
+//     * @return
+//     */
+//    @Bean
+//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+////        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);  // 过期了
+//        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+//        serializer.setObjectMapper(objectMapper);
+//
+//        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory);
+//        template.setDefaultSerializer(serializer);
+//        return template;
+//    }
+
     /**
-     *  使用json序列化
+     *  使用json序列化，使用GenericJackson2JsonRedisSerializer
      * @param redisConnectionFactory
      * @return
      */
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
 
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
@@ -40,7 +81,6 @@ public class RedisConfig extends CachingConfigurerSupport {
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getDefaultSerializer()));
         return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
     }
-
 
     @Bean
     public RedisTemplate<Object, Employee> empRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
